b683c1a7617d0edb9b98bbe0e1146dca5c03b64f
root <root@DESKTOP-NQ3E1IC.localdomain>
2023-07-15 11:01:48 +0800

    step 5

--- __tests__/index.text.js
+++ __tests__/index.text.js
@@ -7,4 +7,8 @@ test('genDiff', () => {
   expect(
     genDiff('__fixtures__/testFile.json', '__fixtures__/testFile2.json')
   ).toEqual(fs.readFileSync('./__fixtures__/testResultIndex', 'utf8'));
+
+  expect(genDiff('__fixtures__/file1.yaml', '__fixtures__/file2.yaml')).toEqual(
+    fs.readFileSync('./__fixtures__/testResultIndex', 'utf8')
+  );
 });
--- makefile
+++ makefile
@@ -8,6 +8,12 @@ install-deps:
 lint:
 		npx eslint .
 
+test:
+	NODE_OPTIONS=--experimental-vm-modules npx jest
 
+test-coverage:
+	NODE_OPTIONS=--experimental-vm-modules npx jest --coverage
+
+	
 publish:
 	npm publish --dry-run
