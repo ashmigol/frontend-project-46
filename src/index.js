6580bc6ac236e6c977526bcb6f2e7d8d02a59f8e
root <root@DESKTOP-NQ3E1IC.localdomain>
2023-07-15 10:07:53 +0800

    step 5

--- src/index.js
+++ src/index.js
@@ -1,16 +1,37 @@
 import fs from 'fs';
 import path from 'path';
+import parse from './parses.js';
+
+const extractFormat = (filePath) => path.extname(filePath);
+//const getData = filePath;
+const fullPath = (filepath) => path.resolve(process.cwd(), filepath);
+//const flpt = '/mnt/c/study/projects/1/frontend-project-46/src/testFile.json';
+
+const getData = (filepath) => {
+  const patch = fullPath(filepath);
+  return parse(fs.readFileSync(patch), extractFormat(filepath));
+};
 
 const gendiff = (filepath1, filepath2) => {
-    
-  const makeObject1 = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), filepath1)));
-  const makeObject2 = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), filepath2)));
-  const arrayJson1 = Object.entries(makeObject1).map(([key, value]) => ({ key, value }));
-  const arrayJson2 = Object.entries(makeObject2).map(([key, value]) => ({ key, value }));
+  const makeObject1 = getData(filepath1);
+  const makeObject2 = getData(filepath2);
+  const arrayJson1 = Object.entries(makeObject1).map(([key, value]) => ({
+    key,
+    value,
+  }));
+  const arrayJson2 = Object.entries(makeObject2).map(([key, value]) => ({
+    key,
+    value,
+  }));
   const result = [];
 
   for (let i = 0; i < arrayJson1.length; i += 1) {
-    if (arrayJson2.some(obj => obj.key === arrayJson1[i].key && obj.value === arrayJson1[i].value)) {
+    if (
+      arrayJson2.some(
+        (obj) =>
+          obj.key === arrayJson1[i].key && obj.value === arrayJson1[i].value
+      )
+    ) {
       result.push(`  ${arrayJson1[i].key}: ${arrayJson1[i].value}`);
     } else {
       result.push(`- ${arrayJson1[i].key}: ${arrayJson1[i].value}`);
@@ -18,7 +39,12 @@ const gendiff = (filepath1, filepath2) => {
   }
 
   for (let j = 0; j < arrayJson2.length; j += 1) {
-    if (!arrayJson1.some(obj => obj.key === arrayJson2[j].key && obj.value === arrayJson2[j].value)) {
+    if (
+      !arrayJson1.some(
+        (obj) =>
+          obj.key === arrayJson2[j].key && obj.value === arrayJson2[j].value
+      )
+    ) {
       result.push(`+ ${arrayJson2[j].key}: ${arrayJson2[j].value}`);
     }
   }
@@ -34,7 +60,6 @@ const gendiff = (filepath1, filepath2) => {
 
   const resultToString = result.join('\n');
   return resultToString;
-
 };
 
 export default gendiff;
---
+++ src/parses.js
@@ -0,0 +1,18 @@
+import yaml from 'js-yaml';
+
+const parse = (data, format) => {
+  switch (format) {
+    case '.json':
+      return JSON.parse(data);
+    case '.yml':
+      return yaml.load(data);
+    case '.yaml':
+      return yaml.load(data);
+    default:
+      throw new Error(
+        `Unsupported file format: '${format}'! Try another format.`
+      );
+  }
+};
+
+export default parse;
