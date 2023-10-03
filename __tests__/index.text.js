80a5a4bf8afcd2b051cc8797cf3fe44d3b7926e3
root <root@DESKTOP-Q49C5QL.localdomain>
2023-10-03 20:21:57 +0800

    step 6

--- __tests__/index.text.js
+++ __tests__/index.text.js
@@ -5,14 +5,14 @@ import path from 'path';
 
 test('genDiff', () => {
   expect(
-    genDiff('../__fixtures__/testFile.json', '../__fixtures__/testFile2.json')
-  ).toEqual(fs.readFileSync('../__fixtures__/testResultIndex', 'utf8'));
+    genDiff('/mnt/f/study/frontend-project-46/__fixtures__/testFile.json', '/mnt/f/study/frontend-project-46/__fixtures__/testFile2.json')
+  ).toEqual(fs.readFileSync('/mnt/f/study/frontend-project-46/__fixtures__/testResultIndex', 'utf8'));
 
-  expect(genDiff('../__fixtures__/file1.yaml', '../__fixtures__/file2.yaml')).toEqual(
-    fs.readFileSync('../__fixtures__/testResultIndex', 'utf8')
+  expect(genDiff('/mnt/f/study/frontend-project-46/__fixtures__/file1.yaml', '/mnt/f/study/frontend-project-46/__fixtures__/file2.yaml')).toEqual(
+    fs.readFileSync('/mnt/f/study/frontend-project-46/__fixtures__/testResultIndex', 'utf8')
   );
 
-  expect(genDiff('../__fixtures__/file.py', '../__fixtures__/file2.yaml')).toEqual(
-    fs.readFileSync('../__fixtures__/testResultIndex', 'utf8')
+  expect(genDiff('/mnt/f/study/frontend-project-46/__fixtures__/file.py', '/mnt/f/study/frontend-project-46/__fixtures__/file2.yaml')).toEqual(
+    fs.readFileSync('/mnt/f/study/frontend-project-46/__fixtures__/testResultIndex', 'utf8')
   );
 });
--- __tests__/parsers.test.js
+++ __tests__/parsers.test.js
@@ -4,17 +4,17 @@ import yaml from 'js-yaml';
 
 
 test('parseFile', () => {
-    const filePath = '../__fixtures__/testFile.json';
-    const expectedJsonData = fs.readFileSync('../__fixtures__/testResultParseJson', 'utf8');
+    const filePath = '/mnt/f/study/frontend-project-46/__fixtures__/testFile.json';
+    const expectedJsonData = fs.readFileSync('/mnt/f/study/frontend-project-46/__fixtures__/testResultParseJson', 'utf8');
     expect(parseFile(expectedJsonData, filePath)).toEqual(JSON.parse(expectedJsonData));
   
-    const yamlFilePath = '../__fixtures__/file1.yaml';
-    const expectedYamlData = fs.readFileSync('../__fixtures__/testResultParseYaml', 'utf8');
+    const yamlFilePath = '/mnt/f/study/frontend-project-46/__fixtures__/file1.yaml';
+    const expectedYamlData = fs.readFileSync('/mnt/f/study/frontend-project-46/__fixtures__/testResultParseYaml', 'utf8');
     expect(parseFile(expectedYamlData, yamlFilePath)).toEqual(yaml.load(expectedYamlData));
   });
 
 test('should throw error for unsupported file format', () => {
-  const unsupportedFilePath = '../__fixtures__/file.py';
+  const unsupportedFilePath = '/mnt/f/study/frontend-project-46/__fixtures__/file.py';
   const unsupportedFileData = fs.readFileSync(unsupportedFilePath, 'utf8');
   expect(() => {
     parseFile(unsupportedFileData, unsupportedFilePath);
