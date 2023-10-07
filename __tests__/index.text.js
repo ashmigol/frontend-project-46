feec204bab414638089961e1db900981f040a17c
root <root@DESKTOP-Q49C5QL.localdomain>
2023-10-07 18:24:19 +0800

    step 6

--- __tests__/index.text.js
+++ __tests__/index.text.js
@@ -13,6 +13,6 @@ test('genDiff', () => {
   );
 
   expect(genDiff('/mnt/f/study/frontend-project-46/__fixtures__/file.py', '/mnt/f/study/frontend-project-46/__fixtures__/file2.yaml')).toEqual(
-    fs.readFileSync('/mnt/f/study/frontend-project-46/__fixtures__/testResultIndex', 'utf8')
+    fs.readFileSync('/mnt/f/study/frontend-project-46/__fixtures__/testResultUnsupp', 'utf8')
   );
 });
