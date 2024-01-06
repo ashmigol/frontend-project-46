c5299678c195f4184dd6be67934537401a0561f2
root <root@DESKTOP-NQ3E1IC.localdomain>
2024-01-06 16:52:29 +0000

    last fixes

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ import getReport from './formatters/index.js';
 const gendiff = (filePath1, filePath2, format = 'stylish') => {
   const getDataFromFile = (filePath) => {
     const data = fs.readFileSync(filePath, 'utf-8');
-    const parsedData = parse(data, filePath);
+    const fileType = filePath.split('.').slice(-1)[0];
+    const parsedData = parse(data, fileType);
     return parsedData;
   };
+  
   const parsedFile1 = getDataFromFile(filePath1);
   const parsedFile2 = getDataFromFile(filePath2);
   const tree = buildTree(parsedFile1, parsedFile2);
