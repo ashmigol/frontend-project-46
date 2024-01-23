79879b7d5ebb400221f47bb866b79ac7b09ece79
root <root@DESKTOP-NQ3E1IC.localdomain>
2024-01-23 13:17:03 +0000

    tests fixes

--- src/buildtree.js
+++ src/buildtree.js
@@ -7,17 +7,17 @@ const buildTree = (object1, object2) => {
 
   return commonKeys.map((key) => {
     if (!_.has(object1, key)) {
-      return { key, status: 'added', value: Object2[key] };
+      return { key, status: 'added', value: object2[key] };
     }
-    if (!_.has(Object2, key)) {
+    if (!_.has(object2, key)) {
       return { key, status: 'deleted', value: object1[key] };
     }
     if (_.isPlainObject(object1[key]) && _.isPlainObject(object2[key])) {
-      return { key, status: 'nested', children: buildTree(object1[key], Object2[key]) };
+      return { key, status: 'nested', children: buildTree(object1[key], object2[key]) };
     }
-    if (!_.isEqual(object1[key], Object2[key])) {
+    if (!_.isEqual(object1[key], object2[key])) {
       return {
-        key, status: 'changed', value1: object1[key], value2: Object2[key],
+        key, status: 'changed', value1: object1[key], value2: object2[key],
       };
     }
     return { key, status: 'unchanged', value: object1[key] };
