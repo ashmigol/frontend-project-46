ba877e7cda79f64285a870ab2c203c039d1bce5a
root <root@DESKTOP-NQ3E1IC.localdomain>
2024-01-23 12:48:58 +0000

    tests fixes

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import parse from './parser.js';
 import buildTree from './buildtree.js';
 import getReport from './formatters/index.js';
 
+const getDataFromFile = (filePath) => {
+  const data = fs.readFileSync(filePath, 'utf-8');
+  const fileType = filePath.split('.').slice(-1)[0];
+  const parsedData = parse(data, fileType);
+  return parsedData;
+};
+
 const gendiff = (filePath1, filePath2, format = 'stylish') => {
-  const getDataFromFile = (filePath) => {
-    const data = fs.readFileSync(filePath, 'utf-8');
-    const fileType = filePath.split('.').slice(-1)[0];
-    const parsedData = parse(data, fileType);
-    return parsedData;
-  };
   const parsedFile1 = getDataFromFile(filePath1);
   const parsedFile2 = getDataFromFile(filePath2);
   const tree = buildTree(parsedFile1, parsedFile2);
